9ed696cb0ae74bfceebbc2215132da3e95f31f74
vbonchevski <viktor@icon-worldwide.com>
2020-01-14 16:07:27 +0200

    while loop init done

--- Conditional and Looping/4. while do - Please start here/code.js
+++ Conditional and Looping/4. while do - Please start here/code.js
@@ -1,3 +1,18 @@
-for(var i = 0; i < 5; i++){
-    console.log(i);
+// While Loops 
+
+var balance = 1000;
+var itemNumber = 0;
+
+while (balance > 0) {
+    var itemPrice = Math.floor(1 + 100*Math.random());
+
+    if(itemPrice <= balance ){
+        itemNumber +=1;
+        balance -= itemPrice;
+
+        console.log(itemNumber + " . Item price: $ " + itemPrice +".");
+
+        console.log("\n Balance is: $" + balance);
+
+    }
 }
