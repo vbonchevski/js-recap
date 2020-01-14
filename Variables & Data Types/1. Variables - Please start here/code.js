9d9ead3c74e1d07c47461cd49cf0eaadb69aca13
vbonchevski <viktor@icon-worldwide.com>
2020-01-14 10:02:06 +0200

    declaring variables done

--- Variables & Data Types/1. Variables - Please start here/code.js
+++ Variables & Data Types/1. Variables - Please start here/code.js
@@ -1,7 +1,23 @@
 // declare a variable
 
 var myVar;
+console.log(myVar + " " + "This is my Variable")
 
-var myNum = 1;
-
+// declare & initialize
+var livesLeft = 3;
 var score;
+
+// reassign a value to var
+
+score = 200;
+livesLeft = 1;
+
+console.log(score, livesLeft);
+
+// constants
+
+const charName = "Mario";
+
+charName = "Not Mario";
+
+console.log(charName); // will give an error as we're trying to reassign a constant 
