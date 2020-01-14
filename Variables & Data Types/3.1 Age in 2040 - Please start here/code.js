a88c73109725b4a5e8c2a55f55acb5eb53e342b5
vbonchevski <viktor@icon-worldwide.com>
2020-01-14 10:51:07 +0200

    challenge complete

---
+++ Variables & Data Types/3.1 Age in 2040 - Please start here/code.js
@@ -0,0 +1,23 @@
+var firstName = prompt('Please enter your first name :');
+var lastName = prompt('Please enter your last name :');
+var birthYear = prompt('Please enter your year of birth:');
+
+var firstSentence = `Hello ${firstName} + ${lastName} .`;
+
+parseInt(birthYear);
+
+var userCurrentAge = 2020 - birthYear;
+var userFutureAge = 2040 - birthYear; 
+
+
+
+var secondSentence = `Your current age is ${userCurrentAge}. You will be ${userFutureAge} years old in 2040`;
+
+var result = firstSentence + "\n" + secondSentence;
+
+console.log(result);
+
+result += ' I wish you a long and happy life';
+
+console.log(`This is after the addition ${result}`);
+
